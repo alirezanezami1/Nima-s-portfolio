ead1d1900d373519061f8e1119c8850daef8ca82
alireza nezami <nezamialireza1022@gmail.com>
2025-07-10 23:57:31 +0330

    create text intro

--- src/components/AboutmeView.vue
+++ src/components/AboutmeView.vue
@@ -4,8 +4,27 @@ import statusBtn from './statusBtn.vue'
 <template>
   <div class="flex flex-col justify-center items-center gap-8 pt-10">
     <!-- /// intro  -->
-    <div class="flex-col justify-center items-center gap-6">
+    <div class="flex flex-col justify-center items-center gap-6">
       <statusBtn />
+
+      <div class="flex flex-col justify-center items-center gap-2 md:gap-8 self-stretch">
+        <div
+          class="px-1 flex flex-col justify-center items-center text-center text-[32px] md:text-[48px] leading-[175%] md:leading-none"
+        >
+          <p class="flex justify-center items-center self-stretch h-[112px]">
+            <span class="text-textColor font-light">Hi I’m</span>
+            <img
+              src="../assets/images/Frame 1.png"
+              class="w-[56px] h-[56px] mx-[10px]"
+              alt="logo"
+            />
+            <span class="text-textColor2 font-semibold">
+              Nima <span class="hidden md:inline">masoumi !</span>
+            </span>
+          </p>
+          <p class="text-textColor2 font-semibold block md:hidden -mt-5">masoumi !</p>
+        </div>
+      </div>
     </div>
 
     <!-- /// about me  -->
