ddbf60479bf1c35365ad8461dde7ba10b972256f
alireza nezami <nezamialireza1022@gmail.com>
2025-07-12 00:19:32 +0330

    compete the aboutMe section

--- src/components/AboutmeView.vue
+++ src/components/AboutmeView.vue
@@ -1,5 +1,17 @@
 <script setup>
 import statusBtn from './statusBtn.vue'
+import ArrowBottom from './icons/ArrowBottom.vue'
+import { computed, ref } from 'vue'
+
+const txtAboutMe = ref(
+  ' I believe that a successful user experience design begins with empathy for the users.Design, for me, is a tool to create positive, lasting human, personal, and social values. As a UX/UI designer, I strive to turn user problems into opportunities for innovation. My goal is to create experiences that not only address challenges but also leave a meaningful and memorable impact on users lives, while achieving business objectives.',
+)
+
+const showMore = ref(false)
+
+const slicedTxt = computed(() => {
+  return `${txtAboutMe.value.slice(0, 98)}...`
+})
 </script>
 <template>
   <div class="flex flex-col justify-center items-center gap-8 pt-3">
@@ -31,5 +43,30 @@ import statusBtn from './statusBtn.vue'
     </div>
 
     <!-- /// about me  -->
+    <div
+      class="flex flex-col justify-center items-start gap-[10px] rounded-2xl border-[1px] border-borderColor2 px-6 pt-6 pb-3"
+    >
+      <div class="flex justify-center items-center flex-col self-stretch gap-4">
+        <div class="flex flex-col justify-center items-center self-stretch gap-4">
+          <p class="text-textColor2 text-ellipsis text-[20px] leading-7 font-semibold">About me</p>
+          <p class="text-textColor text-ellipsis text-[20px] leading-7 font-normal" v-if="showMore">
+            {{ txtAboutMe }}
+          </p>
+          <p
+            class="text-textColor text-ellipsis text-[20px] leading-7 font-normal"
+            v-if="!showMore"
+          >
+            {{ slicedTxt }}
+          </p>
+          <div
+            class="flex justify-center items-center gap-1 h-[40px] rounded-full text-textColor3 text-[16px] font-medium leading-[140%]"
+            @click="showMore = !showMore"
+          >
+            Show <span v-if="!showMore">More</span> <span v-if="showMore">less</span>
+            <ArrowBottom :class="{ 'transform rotate-180': showMore }" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
