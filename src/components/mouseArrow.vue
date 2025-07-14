61a189fe4a759d18e764dc7bc4575dabb0394c65
alireza nezami <nezamialireza1022@gmail.com>
2025-07-14 12:04:39 +0330

    complete mouseArrow comp

---
+++ src/components/mouseArrow.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ArrowBottom from '@/components/icons/ArrowBottom.vue'
+import mouseIcon from '@/components/icons/mouseIcon.vue'
+</script>
+<template>
+  <div class="flex flex-col justify-center py-12 md:py-16">
+    <div class="flex justify-center items-center self-stretch p-2 aspect-square">
+      <mouseIcon class="w-[24px]" />
+    </div>
+    <div class="flex justify-center items-center self-stretch p-2 aspect-square">
+      <ArrowBottom class="w-[24px]" color="#868686" />
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AboutmeView from '@/components/AboutmeView.vue'
+import mouseArrow from '@/components/mouseArrow.vue'
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col justify-center items-center">
     <AboutmeView />
+    <mouseArrow />
   </div>
 </template>
