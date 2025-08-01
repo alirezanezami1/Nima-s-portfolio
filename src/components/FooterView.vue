840720afa0a0d78265d1e09bd92a103ed1aea140
alireza nezami <nezamialireza1022@gmail.com>
2025-08-01 15:32:21 +0330

    responsived

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full text-center font-Roboto flex flex-col justify-center items-center gap-6 h-24 pb-8"
+    class="w-full text-center font-Roboto flex flex-col justify-center items-center gap-6 md:h-24 h-20 pb-8"
   >
     <div class="w-[405px] md:w-[1180px]">
       <svg
@@ -13,7 +13,7 @@
         <path d="M0 1L1180 0.999897" stroke="#868686" stroke-opacity="0.24" />
       </svg>
     </div>
-    <p class="text-[16px] font-medium leading-[150%] text-textColor">
+    <p class="text-[16px] md:w-[405px] w-[345px] font-medium leading-[150%] text-textColor">
       Copyright © 2025 Nima Masoumi . All rights reserved.
     </p>
   </div>
