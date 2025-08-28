3007bcb705be64ea902b983efbbba28b4bb64ca2
alireza nezami <nezamialireza1022@gmail.com>
2025-08-28 21:45:42 +0330

    fix bug

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,14 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// مقادیر مستقیم Supabase
+const supabaseUrl = 'https://twqfeiorchkzqdppurnm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3cWZlaW9yY2hrenFkcHB1cm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1Mzg4MTYsImV4cCI6MjA2NjExNDgxNn0.4lMQLKgP1nw0DXbrlvb4_ZI1qbX33Wkv93XByq3ATaY'
 
+// ایجاد کلاینت Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// تابع کمکی برای بررسی اتصال (همیشه true برمی‌گرداند چون مقادیر مستقیم هستند)
+export const checkSupabaseConnection = () => {
+  return true
+}
