1299d528dc96fccf6b8fd91f905f670a3780dc3f
alireza nezami <nezamialireza1022@gmail.com>
2025-09-09 10:46:51 +0330

    edit style

--- src/components/AboutmeView.vue
+++ src/components/AboutmeView.vue
@@ -28,32 +28,32 @@ const openModal = inject('openModal')
 <template>
   <section id="about">
     <div
-      class="flex flex-col justify-center items-center gap-8 md:gap-8 pt-[150px] md:pt-[186px] relative"
+      class="flex flex-col justify-center items-center gap-8 pt-[150px] sm:pt-[186px] relative"
     >
       <!-- /// intro  -->
-      <div class="flex flex-col justify-center items-center md:gap-[40px]">
+      <div class="flex flex-col justify-center items-center sm:gap-[40px]">
         <statusBtn />
 
-        <div class="flex flex-col justify-center items-center gap-2 md:gap-0 self-stretch">
+        <div class="flex flex-col justify-center items-center gap-2 sm:gap-0 self-stretch">
           <div
-            class="px-1 md:px-0 flex flex-col justify-center items-center text-center text-[32px] md:text-[48px] leading-[175%] md:leading-none"
+            class="px-1 sm:px-0 flex flex-col justify-center items-center text-center text-[32px] sm:text-[48px] leading-[175%] sm:leading-none"
           >
             <p class="flex justify-center items-center self-stretch h-[112px]">
               <span class="text-textColor font-light">Hi I'm</span>
               <img
                 src="../assets/images/Frame 1.png"
-                class="w-[56px] md:w-[80px] h-[56px] md:h-[80px] mx-4"
+                class="w-[56px] sm:w-[80px] h-[56px] sm:h-[80px] mx-4"
                 alt="logo"
               />
               <span class="text-textColor2 font-semibold">
-                Nima <span class="hidden md:inline">masoumi !</span>
+                Nima <span class="hidden sm:inline">masoumi !</span>
               </span>
             </p>
-            <p class="text-textColor2 font-semibold block md:hidden -mt-7">masoumi !</p>
+            <p class="text-textColor2 font-semibold block sm:hidden -mt-7">masoumi !</p>
           </div>
 
           <p
-            class="text-[24px] md:text-[40px] leading-[140%] flex justify-center items-center gap-2 text-textColor font-light text-center"
+            class="text-[24px] sm:text-[40px] leading-[140%] flex justify-center items-center gap-2 text-textColor font-light text-center"
           >
             I'm a <span class="text-textColor2 font-normal">freelance</span>
             <span class="text-textColor3 font-bold">UIUX Designer</span>
@@ -77,38 +77,38 @@ const openModal = inject('openModal')
 
       <!-- /// about me  -->
       <div
-        class="flex flex-col justify-center items-start gap-[10px] rounded-2xl border-[1px] border-borderColor2 px-6 pt-6 pb-3 md:pb-6"
+        class="flex flex-col justify-center items-start gap-[10px] rounded-2xl border-[1px] border-borderColor2 px-6 pt-6 pb-3 sm:pb-6"
       >
         <div class="flex justify-center items-center flex-col self-stretch gap-4">
           <div class="flex flex-col justify-center items-center self-stretch gap-4 text-center">
             <p
-              class="text-textColor2 text-ellipsis text-[20px] md:text-[24px] leading-7 font-semibold"
+              class="text-textColor2 text-ellipsis text-[20px] sm:text-[24px] leading-7 font-semibold"
             >
               About me
             </p>
             <p
-              class="text-textColor text-ellipsis text-[20px] leading-7 font-normal hidden md:block"
+              class="text-textColor text-ellipsis text-[20px] leading-7 font-normal hidden sm:block"
             >
               {{ txtAboutMe }}
             </p>
             <Transition name="aboutTxt" mode="out-in">
               <p
                 key="txtAboutMe"
-                class="text-textColor text-ellipsis text-[20px] leading-7 font-normal block md:hidden"
+                class="text-textColor text-ellipsis text-[20px] leading-7 font-normal block sm:hidden"
                 v-if="showMore"
               >
                 {{ txtAboutMe }}
               </p>
               <p
                 key="slicedTxt"
-                class="text-textColor text-ellipsis text-[20px] leading-7 font-normal block md:hidden"
+                class="text-textColor text-ellipsis text-[20px] leading-7 font-normal block sm:hidden"
                 v-else
               >
                 {{ slicedTxt }}
               </p>
             </Transition>
             <div
-              class="flex md:hidden justify-center items-center gap-1 h-[40px] rounded-full text-textColor3 text-[16px] font-medium leading-[140%]"
+              class="flex sm:hidden justify-center items-center gap-1 h-[40px] rounded-full text-textColor3 text-[16px] font-medium leading-[140%]"
               @click="showMore = !showMore"
             >
               Show <span v-if="!showMore">More</span> <span v-if="showMore">less</span>
@@ -118,7 +118,7 @@ const openModal = inject('openModal')
         </div>
       </div>
 
-      <div class="absolute top-[297px] right-[144px] animate-float-slow hidden md:block">
+      <div class="absolute top-[297px] right-[144px] animate-float-slow hidden sm:block">
         <div class="relative">
           <MouseAboutRight />
           <div class="absolute left-3">
@@ -131,7 +131,7 @@ const openModal = inject('openModal')
         </div>
       </div>
 
-      <div class="absolute top-[210px] left-[164px] animate-float-slow-delayed hidden md:block">
+      <div class="absolute top-[210px] left-[164px] animate-float-slow-delayed hidden sm:block">
         <div class="relative">
           <MouseAboutLeft />
           <div class="absolute -left-11">
